7f5795599bd05477d70fff700e204ff323beeb8f
Stephane Duprat <stephane.duprat@atos.net>
2020-11-24 15:31:15 +0100

    cont

--- plug.ml
+++ plug.ml
@@ -197,6 +197,97 @@ let lines_of_string_list sl =
   in
   List.fold_left ffold 0 sl
 
+(* Comments part *)
+
+let r_lines = ref []
+
+let compute_comment_function (fname:string) (filepath:Filepath.position) lstart lend=
+  Printf.printf "%s %d %d\n" fname lstart lend ;
+  let ffold ((pos1,pos2):cabsloc) str prev_n =
+    if (((Filepath.Normalized.to_pretty_string filepath.pos_path)
+         = (Filepath.Normalized.to_pretty_string pos1.pos_path))
+        && pos1.pos_lnum>=lstart && pos1.pos_lnum<=lend)
+    then
+      prev_n +1
+    else
+      prev_n
+  in
+  Cabshelper.Comments.fold ffold 0
+
+class commentsCabsVisitor = object(self)
+  inherit Cabsvisit.nopCabsVisitor
+
+  method! vdef def =
+    match def with
+      | FUNDEF (_, (_,(function_name,_,_,_)), _, (l1,_), (l2,_)) ->
+        begin
+          let subarray = 
+            try
+              Array.sub (Array.of_list !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1)
+            with Invalid_argument(_) ->
+              Printf.printf "ERROR : %d, %d, %d\n" (List.length !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1);
+              Array.sub (Array.of_list !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1)
+            
+          in
+          let ffold n s =
+            if (Str.string_match (Str.regexp "[a-zA-Z0-9 ;{}]+") s 0) then n+1 else n
+          in
+          let nb_nonempty_line = Array.fold_left ffold 0 subarray in
+          let nb_empty_lines = (l2.pos_lnum - l1.pos_lnum -nb_nonempty_line +1)
+          and nb_comments_function = compute_comment_function function_name l1 l1.pos_lnum l2.pos_lnum
+          in
+          Format.printf "%a %d, %a %d, total %d, diff %d, %d\n" Filepath.Normalized.pp_abs l1.pos_path l1.pos_lnum Filepath.Normalized.pp_abs l2.pos_path l2.pos_lnum nb_nonempty_line (l2.pos_lnum - l1.pos_lnum -nb_nonempty_line +1) nb_comments_function;
+
+          Cil.DoChildren
+        end
+      | _ -> Cil.DoChildren;
+
+
+end
+;;
+
+let rec strech_list l n =
+  if n=0
+  then
+    l
+  else
+    strech_list (List.tl l) (n-1)
+
+let rec init_empty_list n =
+  if n=0
+  then
+    []
+  else
+    "" :: (init_empty_list (n-1))
+
+let compute_empty_lines_per_function () =
+    let cabs_files = Ast.UntypedFiles.get () in
+    let cabs_visitor = new commentsCabsVisitor in
+    List.iter (fun file ->
+        (* 1- compute all lines *)
+        begin
+          let in_channel = open_in (Filepath.Normalized.to_pretty_string (fst file)) in
+          try
+            let r_last_marker = ref 0 in
+            while true do
+              let line = input_line in_channel in
+              let r = Str.regexp "^# \\([0-9]+\\)" in
+              if Str.string_match r line 0
+              then
+                begin
+                  r_last_marker :=  (int_of_string (Str.matched_group 1 line));
+                  r_lines := init_empty_list !r_last_marker;
+                end
+              else
+                r_lines := List.append !r_lines [line]
+            done;
+            (*r_lines := strech_list !r_lines !r_last_marker*)
+          with End_of_file ->
+            close_in in_channel;
+        end;
+    ) cabs_files
+
+
 class c_globals_function =
 object (self)
   inherit Visitor.frama_c_inplace as super
@@ -230,6 +321,26 @@ object (self)
                   in
                   m_function_comment_nb := PairStringMap.add  m_f n !m_function_comment_nb ;
                 );
+
+              begin
+                let subarray = 
+                  try
+                    Array.sub (Array.of_list !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1)
+                  with Invalid_argument(_) ->
+                    Printf.printf "ERROR : %d, %d, %d\n" (List.length !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1);
+                    Array.sub (Array.of_list !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1)
+                      
+                in
+                let ffold n s =
+                  if (Str.string_match (Str.regexp "[a-zA-Z0-9 ;{}]+") s 0) then n+1 else n
+                in
+                let nb_nonempty_line = Array.fold_left ffold 0 subarray in
+                let nb_empty_lines = (l2.pos_lnum - l1.pos_lnum -nb_nonempty_line +1)
+                and nb_comments_function = compute_comment_function vi.vname l1 l1.pos_lnum l2.pos_lnum
+                in
+                Format.printf "%a %d, %a %d, total %d, diff %d, %d\n" Filepath.Normalized.pp_abs l1.pos_path l1.pos_lnum Filepath.Normalized.pp_abs l2.pos_path l2.pos_lnum nb_nonempty_line (l2.pos_lnum - l1.pos_lnum -nb_nonempty_line +1) nb_comments_function;
+              end;
+
               Cil.DoChildren
             )
           else
@@ -580,99 +691,6 @@ let compute_stack ((mf,mm,gf,gm) as prj) =
   StringMap.iter fiter func_to_analyse 
 
 
-(* Comments part *)
-
-let r_lines = ref []
-
-let compute_comment_function (fname:string) (filepath:Filepath.position) lstart lend=
-  Printf.printf "%s %d %d\n" fname lstart lend ;
-  let ffold ((pos1,pos2):cabsloc) str prev_n =
-    if (((Filepath.Normalized.to_pretty_string filepath.pos_path)
-         = (Filepath.Normalized.to_pretty_string pos1.pos_path))
-        && pos1.pos_lnum>=lstart && pos1.pos_lnum<=lend)
-    then
-      prev_n +1
-    else
-      prev_n
-  in
-  Cabshelper.Comments.fold ffold 0
-
-class commentsCabsVisitor = object(self)
-  inherit Cabsvisit.nopCabsVisitor
-
-  method! vdef def =
-    match def with
-      | FUNDEF (_, (_,(function_name,_,_,_)), _, (l1,_), (l2,_)) ->
-        begin
-          let subarray = 
-            try
-              Array.sub (Array.of_list !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1)
-            with Invalid_argument(_) ->
-              Printf.printf "ERROR : %d, %d, %d\n" (List.length !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1);
-              Array.sub (Array.of_list !r_lines) l1.pos_lnum (l2.pos_lnum - l1.pos_lnum +1)
-            
-          in
-          let ffold n s =
-            if (Str.string_match (Str.regexp "[a-zA-Z0-9 ;{}]+") s 0) then n+1 else n
-          in
-          let nb_nonempty_line = Array.fold_left ffold 0 subarray in
-          let nb_empty_lines = (l2.pos_lnum - l1.pos_lnum -nb_nonempty_line +1)
-          and nb_comments_function = compute_comment_function function_name l1 l1.pos_lnum l2.pos_lnum
-          in
-          Format.printf "%a %d, %a %d, total %d, diff %d, %d\n" Filepath.Normalized.pp_abs l1.pos_path l1.pos_lnum Filepath.Normalized.pp_abs l2.pos_path l2.pos_lnum nb_nonempty_line (l2.pos_lnum - l1.pos_lnum -nb_nonempty_line +1) nb_comments_function;
-
-          Cil.DoChildren
-        end
-      | _ -> Cil.DoChildren;
-
-
-end
-;;
-
-let rec strech_list l n =
-  if n=0
-  then
-    l
-  else
-    strech_list (List.tl l) (n-1)
-
-let rec init_empty_list n =
-  if n=0
-  then
-    []
-  else
-    "" :: (init_empty_list (n-1))
-
-let compute_empty_lines_per_function () =
-    let cabs_files = Ast.UntypedFiles.get () in
-    let cabs_visitor = new commentsCabsVisitor in
-    List.iter (fun file ->
-        (* 1- compute all lines *)
-        begin
-          let in_channel = open_in (Filepath.Normalized.to_pretty_string (fst file)) in
-          try
-            let r_last_marker = ref 0 in
-            while true do
-              let line = input_line in_channel in
-              let r = Str.regexp "^# \\([0-9]+\\)" in
-              if Str.string_match r line 0
-              then
-                begin
-                  r_last_marker :=  (int_of_string (Str.matched_group 1 line));
-                  r_lines := init_empty_list !r_last_marker;
-                end
-              else
-                r_lines := List.append !r_lines [line]
-            done;
-            (*r_lines := strech_list !r_lines !r_last_marker*)
-          with End_of_file ->
-            close_in in_channel;
-        end;
-
-        ignore
-	(Cabsvisit.visitCabsFile (cabs_visitor:>Cabsvisit.cabsVisitor) file);
-
-    ) cabs_files
 
 
 
@@ -692,8 +710,8 @@ let run () =
         || (CommentOut.get ()))
     then
       begin
-        ignore (Ast.get());
-        let prj = 
+        let cil_type_file = Ast.get() in
+        let prj =
           let prj=compute_prj ()
           in
             if (CgHead.get ())
@@ -705,8 +723,8 @@ let run () =
         if (CommentOut.get ())
         then
           (
-            ignore (Visitor.visitFramacFile ((new c_globals_function):> Visitor.frama_c_visitor) (Ast.get ()));
             compute_empty_lines_per_function();
+            ignore (Visitor.visitFramacFile ((new c_globals_function):> Visitor.frama_c_visitor) (Ast.get ()));
             PairStringMap.iter (fun (m,f) n -> Printf.printf "%s;%s;%d\n" m f n) !m_function_comment_nb;
           );
 	if (CgAll.get ())
